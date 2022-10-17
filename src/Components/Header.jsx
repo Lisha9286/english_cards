9720fd80e386547aade1602847af39dd45cde0eb
Lisha9286 <lisa@zverev.family>
2022-10-17 15:44:41 +0300

    smth change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './Header.sass';
 import { Link } from 'react-router-dom'
@@ -20,23 +19,6 @@ function Header() {
             </div>
         </div>
             
-=======
-import './Header.sass';
-function Header(props) {
-    return (
-        <div className='header'>
-            <div className='header__logo'>
-                <img className='header__img' alt="" src="./assets/images/english.png" />
-                <div className='header__title'>Practice Words</div>
-            </div>
-            <nav>
-                <ul className='header__menu'>
-                    <li>Log In</li>
-                    <li>Sing Up</li>
-                </ul>
-            </nav>
-        </div>
->>>>>>> c064f9fdb3e5be521d586e4ccc9d1b1721ff2094
     );
 }
 
