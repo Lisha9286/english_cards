0d650bf4ee98d5465a03e0d922f8161e4fb21674
Lisha9286 <lisa@zverev.family>
2022-10-30 23:38:24 +0300

    add validation

--- src/Components/Elements/Modal/Modal.jsx
+++ src/Components/Elements/Modal/Modal.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
 import './Modal.sass'
 import PutServices from '../../../Api/PutServices'
 
+
 export default function Modal({ options, closeModal }) {
-    
+
+    const [isEmptyEng, setEmptyEng] = useState(false)
+    const [isEmptyTranslate, setEmptyTranslate] = useState(false)
+    const [isEmptyTranscript, setEmptyTranscript] = useState(false)
+    const [errorMsg, setErrorMsg] = useState('field cannot be empty')
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'english': 
+                if (e.target.value === "")
+                    setEmptyEng(true)
+            break
+            case 'translate': 
+                if (e.target.value === "")
+            setEmptyTranslate(true)
+            break
+            case 'transcription': 
+                if (e.target.value === "")
+            setEmptyTranscript(true)
+            break
+        }
+    }
 
     function getValueFormEnglish(e) {
         options.setFormEnglish(e.target.value)
@@ -29,15 +52,10 @@ export default function Modal({ options, closeModal }) {
     //редактирование карточек
     function ediCard() {
         PutServices.putCard(options.formEnglish, options.formTranslate, options.formTranscript, options.card[0].id)
-        // if (options.card[0].englishWord = false) {
-        //     options.setFormEnglish()
-        // }
         options.setModal(false)
         options.setRerenderCard(!options.rerenderCard)
         cleanValue()
     }
-
-
 
     function cleanValue() {
         englishValue = ""
@@ -52,21 +70,27 @@ export default function Modal({ options, closeModal }) {
                 <div className="modal__content">
                     <div>
                     <form> 
-                <div className="modal__form">
-                    <div><input onChange={getValueFormEnglish} defaultValue={englishValue} className="modal__input" type="text" placeholder='english' />
-                    </div>
-                    <div><input onChange={getValueFormTranslate} defaultValue={translateValue} className="modal__input" type="text" placeholder='translate' />
-                    </div>
-                    <div><input onChange={getValueFormTranscript} defaultValue={transcriptionValue} className="modal__input" type="text" placeholder='transcription' />
-                    </div>
-                </div>
-            </form>
+                        <div className="modal__form">
+                            <div>
+                                    {(isEmptyEng && errorMsg) && <div className='modal__error'>{errorMsg}</div>}
+                                <input onBlur={e => blurHandler(e)} onChange={getValueFormEnglish} defaultValue={englishValue} className="modal__input" type="text" placeholder="english" name="english" />
+                            </div>
+                            <div>
+                            {(isEmptyTranslate && errorMsg) && <div className='modal__error'>{errorMsg}</div>}
+                                <input onBlur={e => blurHandler(e)} onChange={getValueFormTranslate} defaultValue={translateValue} className="modal__input" type="text" placeholder="translate" name="translate" />
+                            </div>
+                            <div>
+                            {(isEmptyTranscript && errorMsg) && <div className='modal__error'>{errorMsg}</div>}
+                                <input onBlur={e => blurHandler(e)} onChange={getValueFormTranscript} defaultValue={transcriptionValue} className="modal__input" type="text" placeholder="transcription" name="transcription" />
+                            </div>
+                        </div>
+                    </form>
                     </div>
                     <div className="modal__btns">
-                        <button onClick={ediCard}>save</button>
+                        {/* <button disabled={!formValid} onClick={ediCard}>save</button> */}
                         <button onClick={() => {
                             closeModal(false)
-                            // cleanValue()
+                            cleanValue()
                         }}>cancel</button>
                     </div>
                 </div>
